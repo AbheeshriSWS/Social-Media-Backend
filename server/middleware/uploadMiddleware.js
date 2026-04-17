2f3bad747613e5499c71fe4648987ab455e618b7
AbheeshriSWS <abheeshri.sws10@gmail.com>
2026-04-17 12:04:13 +0530

    cloudinary added for img storage

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -1,12 +1,13 @@
 const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../config/cloudinary");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "social-app",
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
   },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  }
 });
 
 const upload = multer({ storage });
